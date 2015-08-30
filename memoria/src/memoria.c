1ac13fb5f26cd5e04fd0b66191560392f37ceba3
Alejandro Elustondo <aleelus@hotmail.com>
2015-08-30 18:48:22 -0300

    Memoria - 1er CP

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -316,3 +316,301 @@ int conectarSWAP() {
 	freeaddrinfo(serverInfo);	// No lo necesitamos mas
 	return conexionOk;
 }
+
+
+int conexionASwap(){
+	//int cont;
+	//int bytesRecibidos,cantRafaga=1,tamanio;
+	//char*buffer = string_new();
+	//char*bufferR = string_new();
+	//char*bufferE = string_new();
+	//char*aux;
+
+	if(conectarSWAP()){
+		printf("Se conecto a SWAP\n");
+		//ENVIO a FILESYSTEM(la segunda rafaga)
+		//31210127.0.0.1143000
+		//string_append(&buffer,"31");
+		//aux=obtenerSubBuffer(g_Ip_Nodo);
+		//string_append(&buffer,aux);
+		//aux=obtenerSubBuffer(string_itoa(g_Puerto_Nodo));
+		//string_append(&buffer,aux);
+		//aux=obtenerSubBuffer(LuToChar(tamanio_archivo(g_Archivo_Bin)));
+		//string_append(&buffer,aux);
+
+		//string_append(&bufferE,"3");
+		//cont = cuentaDigitos(strlen(buffer));
+		//string_append(&bufferE,string_itoa(cont));
+		//string_append(&bufferE,string_itoa(strlen(buffer)));
+
+		//Primera Rafaga
+		//sem_wait(&semCon);
+		//EnviarDatos(socket_Fs,bufferE, strlen(bufferE));
+		//sem_post(&semCon);
+		//bufferR = RecibirDatos(socket_Fs,bufferR, &bytesRecibidos,&cantRafaga,&tamanio);
+		//Recibo respuesta de FS
+		//if(bufferR!=NULL){
+			//Segunda Rafaga
+			//sem_wait(&semCon);
+			//EnviarDatos(socket_Fs,buffer, strlen(buffer));
+			//sem_post(&semCon);
+			//bufferR = RecibirDatos(socket_Fs,bufferR, &bytesRecibidos,&cantRafaga,&tamanio);
+			//printf("\nNodo Conectado al Fs!");
+		//} else {
+			//printf("No se pudo conectar al FS\n");
+		//}
+		//free(buffer);
+		//free(bufferR);
+		//free(bufferE);
+		return 1;
+	}
+	return 0;
+}
+
+
+void CerrarSocket(int socket) {
+	close(socket);
+	//Traza("SOCKET SE CIERRA: (%d).", socket);
+	log_trace(logger, "SOCKET SE CIERRA: (%d).", socket);
+}
+
+void ErrorFatal(const char* mensaje, ...) {
+	char* nuevo;
+	va_list arguments;
+	va_start(arguments, mensaje);
+	nuevo = string_from_vformat(mensaje, arguments);
+	printf("\nERROR FATAL--> %s \n", nuevo);
+	log_error(logger, "\nERROR FATAL--> %s \n", nuevo);
+	char fin;
+
+	printf(
+			"El programa se cerrara. Presione ENTER para finalizar la ejecución.");
+	fin = scanf("%c", &fin);
+
+	va_end(arguments);
+	if (nuevo != NULL )
+		free(nuevo);
+	exit(EXIT_FAILURE);
+}
+
+int ObtenerComandoMSJ(char* buffer) {
+//Hay que obtener el comando dado el buffer.
+//El comando está dado por el primer caracter, que tiene que ser un número.
+	return PosicionDeBufferAInt(buffer, 0);
+}
+
+int PosicionDeBufferAInt(char* buffer, int posicion) {
+	int logitudBuffer = 0;
+	logitudBuffer = strlen(buffer);
+
+	if (logitudBuffer <= posicion)
+		return 0;
+	else
+		return ChartToInt(buffer[posicion]);
+}
+
+int ChartToInt(char x) {
+	int numero = 0;
+	char * aux = string_new();
+	string_append_with_format(&aux, "%c", x);
+	//char* aux = malloc(1 * sizeof(char));
+	//sprintf(aux, "%c", x);
+	numero = strtol(aux, (char **) NULL, 10);
+
+	if (aux != NULL )
+		free(aux);
+	return numero;
+}
+
+long unsigned RecibirDatos(int socket, char **buffer) {
+	long bytesRecibidos = 0,tamanioBuffer=0,bytesEnviados;
+	char *bufferAux= malloc(1);
+	int posicion=1;
+	memset(bufferAux,0,1);
+
+	bufferAux = realloc(bufferAux,BUFFERSIZE * sizeof(char)+1);
+
+	memset(bufferAux, 0, BUFFERSIZE * sizeof(char)+1); //-> llenamos el bufferAux con barras ceros.
+
+	if ((bytesRecibidos = bytesRecibidos+recv(socket, bufferAux, BUFFERSIZE, 0)) == -1) {
+		Error("Ocurrio un error al intentar recibir datos desde uno de los clientes. Socket: %d",socket);
+	}
+
+	if(bytesRecibidos>0) {
+		bytesEnviados = send(socket, "Ok", strlen("Ok"), 0);
+		if(bytesEnviados<=0) return 0;
+	} else return 0;
+
+	tamanioBuffer = atoi(DigitosNombreArchivo(bufferAux,&posicion));
+
+	free(bufferAux);
+
+	bufferAux = malloc(tamanioBuffer+1);
+	*buffer = malloc(tamanioBuffer+10);
+	memset(bufferAux,0,tamanioBuffer+1);
+	memset(*buffer,0,tamanioBuffer+10);
+
+	ssize_t numBytesRecv = 0;
+
+	long unsigned pos=0;
+	do{
+		numBytesRecv = recv(socket, bufferAux, tamanioBuffer, 0);
+		if ( numBytesRecv < 0)
+			printf("ERROR\n");
+		//printf("Recibido:%lu\n",pos);
+		memcpy((*buffer+pos),bufferAux,numBytesRecv);
+		memset(bufferAux,0,tamanioBuffer+1);
+		pos = pos + numBytesRecv;
+	}while (pos < tamanioBuffer);
+
+	log_trace(logger, "RECIBO DATOS. socket: %d. tamanio buffer:%lu", socket,tamanioBuffer);
+	return tamanioBuffer;
+}
+
+
+char* DigitosNombreArchivo(char *buffer,int *posicion){
+
+	char *nombreArch;
+	int digito=0,i=0,j=0,algo=0,aux=0,x=0;
+
+	digito=PosicionDeBufferAInt(buffer,*posicion);
+	for(i=1;i<=digito;i++){
+		algo=PosicionDeBufferAInt(buffer,*posicion+i);
+		aux=aux*10+algo;
+	}
+	nombreArch = malloc(aux+1);
+	for(j=*posicion+i;j<*posicion+i+aux;j++){
+		nombreArch[x]=buffer[j];
+		x++;
+	}
+	nombreArch[x]='\0';
+	*posicion=*posicion+i+aux;
+	return nombreArch;
+}
+
+long unsigned EnviarDatos(int socket, char *buffer, long unsigned tamanioBuffer) {
+
+	int bytecount,bytesRecibidos;
+	long unsigned cantEnviados=0;
+	char * bufferE = string_new(),*bufferR=malloc(BUFFERSIZE);
+	memset(bufferR,0,BUFFERSIZE);
+
+	string_append(&bufferE,YO);
+
+	string_append(&bufferE,obtenerSubBuffer(string_itoa(tamanioBuffer)));
+
+	if ((bytecount = send(socket,bufferE,strlen(bufferE), 0)) == -1){
+		Error("No puedo enviar información a al cliente. Socket: %d", socket);
+		return 0;
+	}
+
+	if ((bytesRecibidos = recv(socket, bufferR, BUFFERSIZE, 0)) == -1) {
+		Error("Ocurrio un error al intentar recibir datos desde uno de los clientes. Socket: %d",socket);
+	}
+
+	long unsigned n,bytesleft=tamanioBuffer;
+
+	while(cantEnviados < tamanioBuffer) {
+		n = send(socket, buffer+cantEnviados, bytesleft, 0);
+		if (n == -1){
+			Error("Fallo al enviar a Socket: %d,",socket);
+			return 0;
+		}
+		cantEnviados += n;
+		bytesleft -= n;
+		//printf("Cantidad Enviada :%lu\n",n);
+	}
+	if(cantEnviados!=tamanioBuffer) return 0;
+	log_info(logger, "ENVIO DATOS. socket: %d. Cantidad Enviada:%lu ",socket,tamanioBuffer);
+	return tamanioBuffer;
+}
+
+#if 1 // METODOS CONFIGURACION //
+void LevantarConfig() {
+	t_config* config = config_create(PATH_CONFIG);
+	// Nos fijamos si el archivo de conf. pudo ser leido y si tiene los parametros
+	if (config->properties->table_current_size != 0) {
+
+		// Puerto de escucha
+		if (config_has_property(config, "PUERTO_ESCUCHA")) {
+			g_Puerto = config_get_int_value(config, "PUERTO_ESCUCHA");
+		} else
+			Error("No se pudo leer el parametro PUERTO_ESCUCHA");
+		if (config_has_property(config, "IP_SWAP")) {
+			g_Ip_Swap = config_get_string_value(config, "IP_SWAP");
+		} else
+			Error("No se pudo leer el parametro IP_SWAP");
+		if (config_has_property(config, "PUERTO_SWAP")) {
+			g_Puerto_Swap = config_get_string_value(config, "PUERTO_SWAP");
+		} else
+			Error("No se pudo leer el parametro PUERTO_SWAP");
+		if (config_has_property(config, "MAXIMO_MARCOS_POR_PROCESO")) {
+			g_Maximo_Marcos_Por_Proceso = config_get_int_value(config, "MAXIMO_MARCOS_POR_PROCESO");
+		} else
+			Error("No se pudo leer el parametro MAXIMO_MARCOS_POR_PROCESO");
+		if (config_has_property(config, "CANTIDAD_MARCOS")) {
+			g_Cantidad_Marcos = config_get_int_value(config, "CANTIDAD_MARCOS");
+		} else
+			Error("No se pudo leer el parametro CANTIDAD_MARCOS");
+		if (config_has_property(config, "TAMANIO_MARCO")) {
+			g_Tamanio_Marco = config_get_int_value(config, "TAMANIO_MARCO");
+		} else
+			Error("No se pudo leer el parametro TAMANIO_MARCO");
+		if (config_has_property(config, "ENTRADAS_TLB")) {
+			g_Entradas_TLB = config_get_int_value(config, "ENTRADAS_TLB");
+		} else
+			Error("No se pudo leer el parametro ENTRADAS_TLB");
+		if (config_has_property(config, "TLB_HABILITADA")) {
+			g_TLB_Habilitada = config_get_string_value(config, "TLB_HABILITADA");
+		} else
+			Error("No se pudo leer el parametro TLB_HABILITADA");
+		if (config_has_property(config, "RETARDO_MEMORIA")) {
+			g_Retardo_Memoria = config_get_int_value(config, "RETARDO_MEMORIA");
+		} else
+			Error("No se pudo leer el parametro RETARDO_MEMORIA");
+	} else {
+		ErrorFatal("No se pudo abrir el archivo de configuracion");
+	}
+	if (config != NULL ) {
+		free(config);
+	}
+}
+
+#endif
+
+#if 1 // METODOS MANEJO DE ERRORES //
+void Error(const char* mensaje, ...) {
+	char* nuevo;
+	va_list arguments;
+	va_start(arguments, mensaje);
+	nuevo = string_from_vformat(mensaje, arguments);
+
+	fprintf(stderr, "\nERROR: %s\n", nuevo);
+	log_error(logger, "%s", nuevo);
+
+	va_end(arguments);
+	if (nuevo != NULL )
+		free(nuevo);
+}
+#endif
+
+char* obtenerSubBuffer(char *nombre){
+	// Esta funcion recibe un nombre y devuelve ese nombre de acuerdo al protocolo. Ej: carlos ------> 16carlos
+	char *aux=string_new();
+	int tamanioNombre=0;
+	float tam=0;
+	int cont=0;
+
+	tamanioNombre=strlen(nombre);
+	tam=tamanioNombre;
+	while(tam>=1){
+		tam=tam/10;
+		cont++;
+	}
+	string_append(&aux,string_itoa(cont));
+	string_append(&aux,string_itoa(tamanioNombre));
+	string_append(&aux,nombre);
+
+	return aux;
+}
+
